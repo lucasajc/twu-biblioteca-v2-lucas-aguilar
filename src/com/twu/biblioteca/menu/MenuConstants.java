53c11042177a8c4e944586f004ff1ae735fbdde0
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2019-10-08 15:14:48 -0300

    [2.2] Add checkout and return movies to menu

--- src/com/twu/biblioteca/menu/MenuConstants.java
+++ src/com/twu/biblioteca/menu/MenuConstants.java
@@ -5,5 +5,7 @@ public class MenuConstants {
     public static final int LIST_MOVIES_KEY = 2;
     public static final int CHECKOUT_BOOK_KEY = 3;
     public static final int RETURN_BOOK_KEY = 4;
+    public static final int CHECKOUT_MOVIE_KEY = 5;
+    public static final int RETURN_MOVIE_KEY = 6;
     public static final int EXIT_APPLICATION_KEY = 0;
 }
